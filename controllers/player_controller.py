0894ba64d0177a11e951825c4017966d2c22273d
Timur Pavlov <timurpavlov281@gmail.com>
2025-12-11 23:16:00 +0300

    Новый класс
    Создаем новый класс для управления плеером

---
+++ controllers/player_controller.py
@@ -0,0 +1,10 @@
+"""
+Контроллер видеоплеера
+"""
+
+import threading
+import time
+from tkinter import messagebox, simpledialog
+
+class PlayerController:
+    """Контроллер для управления видеоплеером"""
