6bf229b27614ee0db812415679d85ffff2fd127b
Timur Pavlov <timurpavlov281@gmail.com>
2025-12-11 22:58:25 +0300

    Создал новый файл
    Создал основной файл и импортировал библиотеки

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Главный файл приложения Video Player
+"""
+
+import tkinter as tk
+from views.main_window import MainWindow
+from controllers.player_controller import PlayerController
+from models.video_player import VideoPlayer
